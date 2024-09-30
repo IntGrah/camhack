2501decd5387d7c5ce53de0e15b7d74b31aad7ff
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-09-30 21:34:52 +0100

    Responsive design for mobile

--- static/handlers.js
+++ static/handlers.js
@@ -1,5 +1,19 @@
+// When FAQ question clicked, toggle answer
 document.querySelectorAll(".faq-question .faq-toggle").forEach((faqToggle) => {
     faqToggle.addEventListener("click", () => {
         faqToggle.parentElement.classList.toggle("expanded");
+    });
+});
+
+// When hamburger icon clicked, toggle menu
+document.getElementById("header-hamburger").addEventListener("click", () => {
+    document.getElementById("header-hamburger").classList.toggle("expanded");
+    document.getElementById("header-menu-container").classList.toggle("expanded");
+});
+
+// When menu link clicked, close menu
+document.querySelectorAll("#header-menu a").forEach((menuLink) => {
+    menuLink.addEventListener("click", () => {
+        document.getElementById("header-menu-container").classList.remove("expanded");
     });
 });
