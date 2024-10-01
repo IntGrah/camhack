6299990f3fb5750c523e57726c11cc3c918f8346
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-10-02 00:44:09 +0100

    FAQ content

--- src/lib/components/FAQ.svelte
+++ src/lib/components/FAQ.svelte
@@ -6,41 +6,49 @@
     <h2 class="mb-4 text-4xl font-bold">FAQ</h2>
     <FAQuestion>
         <span slot="question">What is a hackathon?</span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-        assumenda ducimus debitis reiciendis dolore dolores quae dolorum
-        eius? Tempora nemo beatae quisquam error vel. Voluptatem laborum
-        ad excepturi rem porro?
+        A hackathon is an event where people collaborate to make cool stuff! It usually
+        runs over a weekend and is a great way to learn new skills, meet new people,
+        and have fun!
+    </FAQuestion>
+    <FAQuestion>
+        <span slot="question">Where is it?</span>
+        Cam Hack will be in the <a href="https://maps.app.goo.gl/185n4bsvv7AXw3FM7">William Gates Building</a>, 15 JJ Thomson Avenue CB3 0FD.
     </FAQuestion>
     <FAQuestion>
         <span slot="question">What teams are allowed?</span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-        assumenda ducimus debitis reiciendis dolore dolores quae dolorum
-        eius? Tempora nemo beatae quisquam error vel. Voluptatem laborum
-        ad excepturi rem porro?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis assumenda
+        ducimus debitis reiciendis dolore dolores quae dolorum eius? Tempora nemo
+        beatae quisquam error vel. Voluptatem laborum ad excepturi rem porro?
     </FAQuestion>
     <FAQuestion>
         <span slot="question">How do I sign up?</span>
-        Fill out the <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="hyperlink">sign up form</a> by &lt;date&gt;.
+        Fill out the
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="hyperlink"
+            >sign up form</a
+        > by TODO_INSERT_DATE.
     </FAQuestion>
     <FAQuestion>
-        <span slot="question">What is the prize?</span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptate
-        vel exercitationem dignissimos ullam magnam, quisquam nobis sed voluptatem
-        similique laboriosam, nostrum perspiciatis nisi! Magni quaerat ipsum
-        vitae natus rem!
+        <span slot="question">What prizes are there?</span>
+        TODO Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptate vel
+        exercitationem dignissimos ullam magnam, quisquam nobis sed voluptatem similique
+        laboriosam, nostrum perspiciatis nisi! Magni quaerat ipsum vitae natus rem!
+    </FAQuestion>
+    <FAQuestion>
+        <span slot="question">Will there be food?</span>
+        Yes! We will provide meals throughout the event, from Saturday breakfast
+        to Sunday lunch. Please let us know if you have any dietary requirements
+        in the sign up form. TODO
     </FAQuestion>
     <FAQuestion>
         <span slot="question">Bggrl barg plob!?</span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptate
-        vel exercitationem dignissimos ullam magnam, quisquam nobis sed voluptatem
-        similique laboriosam, nostrum perspiciatis nisi! Magni quaerat ipsum
-        vitae natus rem!
+        TODO Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptate vel
+        exercitationem dignissimos ullam magnam, quisquam nobis sed voluptatem similique
+        laboriosam, nostrum perspiciatis nisi! Magni quaerat ipsum vitae natus rem!
     </FAQuestion>
     <FAQuestion>
         <span slot="question">You didn't answer my questions!</span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-        facere error sunt quas ratione obcaecati vel perspiciatis debitis
-        quos. Dolores quo quae fuga accusamus culpa illo earum cupiditate
-        eaque tempore?
+        TODO Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores facere error
+        sunt quas ratione obcaecati vel perspiciatis debitis quos. Dolores quo quae
+        fuga accusamus culpa illo earum cupiditate eaque tempore?
     </FAQuestion>
 </div>
