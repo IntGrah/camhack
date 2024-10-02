7e4ff48f11bac309316148bbf6f81c509d0d4223
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-10-02 11:08:22 +0100

    Make menu automatically close

--- src/lib/components/MobileMenu.svelte
+++ src/lib/components/MobileMenu.svelte
@@ -2,16 +2,29 @@
     import { slide } from "svelte/transition";
 
     export let expanded: boolean;
+
+    function closeMenu() {
+        expanded = false;
+    }
 </script>
 
 {#if expanded}
-    <div class="md:hidden absolute w-full select-none drop-shadow-2xl" transition:slide>
+    <div
+        class="md:hidden absolute w-full select-none drop-shadow-2xl"
+        transition:slide
+    >
         <menu id="mobile-menu">
-            <a class="block px-8 py-4 decoration-0 text-gray-800 transition-colors bg-white active:bg-orange-200" href="/">Home</a>
-            <a class="block px-8 py-4 decoration-0 text-gray-800 transition-colors bg-white active:bg-orange-200" href="/#about">About</a>
-            <a class="block px-8 py-4 decoration-0 text-gray-800 transition-colors bg-white active:bg-orange-200" href="/#schedule">Schedule</a>
-            <a class="block px-8 py-4 decoration-0 text-gray-800 transition-colors bg-white active:bg-orange-200" href="/#rules">Rules</a>
-            <a class="block px-8 py-4 decoration-0 text-gray-800 transition-colors bg-white active:bg-orange-200" href="/#faq">FAQ</a>
+            <a class="mobile-menu-link" on:click={closeMenu} href="/">Home</a>
+            <a class="mobile-menu-link" on:click={closeMenu} href="/#about">About</a>
+            <a class="mobile-menu-link" on:click={closeMenu} href="/#schedule">Schedule</a>
+            <a class="mobile-menu-link" on:click={closeMenu} href="/#rules">Rules</a>
+            <a class="mobile-menu-link" on:click={closeMenu} href="/#faq">FAQ</a>
         </menu>
     </div>
 {/if}
+
+<style lang="postcss">
+    a.mobile-menu-link {
+        @apply block px-8 py-4 decoration-0 text-gray-800 transition-colors bg-white active:bg-orange-200;
+    }
+</style>
