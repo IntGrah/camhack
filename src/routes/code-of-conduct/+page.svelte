162b31ceb95b3dfa3db91588246284644d9b87bb
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-10-02 11:03:01 +0100

    Code of conduct

--- src/routes/code-of-conduct/+page.svelte
+++ src/routes/code-of-conduct/+page.svelte
@@ -1,17 +1,21 @@
-<section class="px-8 py-4">
+<script>
+    import Organisers from "$lib/components/Organisers.svelte";
+</script>
+
+<section class="px-8 py-4 text-lg">
     <div class="max-w-7xl mx-auto">
         <h1 class="mb-8 text-4xl font-bold">Code of Conduct</h1>
-        <p class="mb-4 text-xl">
+        <p class="mb-4">
             Cam Hack is committed to providing a safe, inclusive, and welcoming
             experience for everyone. This code of conduct outlines our
             expectations for all participants and attendees.
         </p>
-        <p class="mb-4 text-xl">
+        <p class="mb-4">
             We do not tolerate any form of harassment, discrimination, or
             exclusion of any form. In particular, we do not tolerate
             discrimination based on
         </p>
-        <ul class="pl-8 text-xl list-disc">
+        <ul class="mb-4 pl-8 list-disc">
             <li>race, ethnicity, or nationality</li>
             <li>gender identity, expression, or sexual orientation</li>
             <li>age</li>
@@ -19,5 +23,14 @@
             <li>physical appearance or body size</li>
             <li>religion or belief system.</li>
         </ul>
+        <p class="mb-4">
+            If you feel you are being harassed by someone else at the hackathon,
+            please come talk to a member of the committee and we will handle the
+            situation accordingly.
+        </p>
+        <p class="mb-4">
+            Participants who break these rules may be disqualified at the
+            discretion of the committee.
+        </p>
     </div>
 </section>
