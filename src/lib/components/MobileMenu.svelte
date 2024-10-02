dc0234be6147ee171802d1799fd713e3b70de2ea
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-10-02 17:41:41 +0100

    Fix mobile menu expand

--- src/lib/components/MobileMenu.svelte
+++ src/lib/components/MobileMenu.svelte
@@ -12,23 +12,21 @@
     $: if ($navigating) closeMenu();
 </script>
 
-{#if expanded}
-    <div
-        class="md:hidden absolute w-full select-none drop-shadow-2xl"
-        transition:slide
-    >
-        <menu id="mobile-menu">
-            <a on:click={closeMenu} href="/">Home</a>
-            <a on:click={closeMenu} href="/#about">About</a>
-            <a on:click={closeMenu} href="/#schedule">Schedule</a>
-            <a on:click={closeMenu} href="/#rules">Rules</a>
-            <a on:click={closeMenu} href="/#faq">FAQ</a>
-        </menu>
-    </div>
-{/if}
+<div
+    class="md:hidden absolute w-full select-none drop-shadow-2xl"
+    transition:slide
+>
+    <menu>
+        <a on:click={closeMenu} href="/#about">About</a>
+        <a on:click={closeMenu} href="/#schedule">Schedule</a>
+        <a on:click={closeMenu} href="/#rules">Rules</a>
+        <a on:click={closeMenu} href="/#faq">FAQ</a>
+        <a on:click={closeMenu} href="/signup">Sign up</a>
+    </menu>
+</div>
 
 <style lang="postcss">
     a {
-        @apply block px-8 py-4 decoration-0 text-gray-800 transition-colors bg-white active:bg-orange-200;
+        @apply block px-8 py-4 text-gray-800 transition-colors bg-white active:bg-orange-200;
     }
 </style>
