9396fbfa696f512bc2e54883c0a425f59b2169d1
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-10-02 16:20:36 +0100

    Add committee member

--- src/lib/components/Organisers.svelte
+++ src/lib/components/Organisers.svelte
@@ -8,11 +8,12 @@
             <p class="mb-2">Oliver Greenwood</p>
             <p class="mb-2">Swarnim Jain</p>
             <p class="mb-2">Eugene Kang</p>
+            <p class="mb-2">Carol Khor</p>
         </div>
         <div class="flex-1">
-            <p class="mb-2">Carol Khor</p>
             <p class="mb-2">Varun Latthe</p>
             <p class="mb-2">Dylan Moss</p>
+            <p class="mb-2">Sophie Ring</p>
             <p class="mb-2">Ved Rungta</p>
             <p class="mb-2">Ram Vinjamuri</p>
             <p class="mb-2">Dan Wendon&ndash;Blixrud</p>
