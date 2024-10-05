15047dd0680d6cb5ae4c150015cda03a3aa6d537
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-10-05 01:13:10 +0100

    FAQ content

--- src/lib/components/FAQ.svelte
+++ src/lib/components/FAQ.svelte
@@ -5,6 +5,11 @@
 <div class="max-w-7xl mx-auto">
     <h2 class="mb-4 text-4xl font-bold">FAQ</h2>
     <FAQuestion>
+        <span slot="question">Can I participate online?</span>
+        No, unfortunately Cam Hack 2024 is an in-person event. We hope to see you
+        at the Computer Lab!
+    </FAQuestion>
+    <FAQuestion>
         <span slot="question">What is a hackathon?</span>
         A hackathon is an event where people collaborate to make cool stuff! It usually
         runs over a weekend and is a great way to learn new skills, meet new people,
@@ -12,17 +17,21 @@
     </FAQuestion>
     <FAQuestion>
         <span slot="question">Where is it?</span>
-        Cam Hack 2024 will be in the
+        Cam Hack 2024 will be in the Computer Laboratory, which is at the
         <a
             href="https://maps.app.goo.gl/185n4bsvv7AXw3FM7"
-            class="theme-hyperlink">William Gates Building</a
-        >, 15 JJ Thomson Avenue CB3 0FD.
+            class="theme-hyperlink"
+            >William Gates Building, 15 JJ Thomson Avenue, CB3 0FD</a
+        >.
     </FAQuestion>
     <FAQuestion>
         <span slot="question">What teams are allowed?</span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis assumenda
-        ducimus debitis reiciendis dolore dolores quae dolorum eius? Tempora nemo
-        beatae quisquam error vel. Voluptatem laborum ad excepturi rem porro?
+        Team sizes can be up to 5 people. Any bigger, and we may have to split you
+        into smaller groups. If you don't have a team yet, don't hesitate to look
+        around in our
+        <a target="_blank" class="theme-hyperlink" href="/discord"
+            >Discord server</a
+        >!
     </FAQuestion>
     <FAQuestion>
         <span slot="question">How do I sign up?</span>
@@ -46,16 +55,16 @@
     <FAQuestion>
         <span slot="question">Will there be food?</span>
         Yes! We will provide meals throughout the event, from Saturday breakfast
-        to Sunday lunch. Please let us know if you have any dietary requirements
+        to Sunday dinner. Please let us know if you have any dietary requirements
         in the sign up form. TODO
     </FAQuestion>
     <FAQuestion>
-        <span slot="question">Bggrl barg plob!?</span>
-        Bgrl plorb! Barlgrl bgrl plorb, flurb glrb. Uh-oh, I think I've been hacked!
+        <span slot="question">Can I submit to multiple tracks?</span>
+        Yes, but each product can only be submitted to one track.
     </FAQuestion>
     <FAQuestion>
         <span slot="question">You didn't answer my questions!</span>
-        Ask us anything in our
+        Sorry! But you can ask us anything in our
         <a target="_blank" href="/discord" class="theme-hyperlink"
             >Discord server</a
         >! You can also email us at TODO, or speak to us in person at the event.
