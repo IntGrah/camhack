d4f68d31bb7543e3577bcdef3fdcf71780f12b9d
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-10-10 17:55:59 +0100

    Terms of Service

---
+++ src/routes/terms-of-service/+page.svelte
@@ -0,0 +1,46 @@
+<svelte:head>
+    <title>Terms of Service &bull; Cam Hack 2024</title>
+</svelte:head>
+
+<section>
+    <div class="max-w-7xl mx-auto">
+        <h2>Terms of Service</h2>
+        <p>
+            <em>
+                Last Updated: <time datetime="2024-10-10">10 October 2024</time>
+            </em>
+        </p>
+        <h3>1. Acceptance of Terms</h3>
+        <p>
+            By registering for and participating in this event, you acknowledge
+            that you have read and understood these Terms and agree to them.
+        </p>
+        <h3>2. Code of Conduct</h3>
+        <p>
+            All participants are required to adhere to our <a
+                href="/code-of-conduct"
+                class="theme-hyperlink">Code of Conduct</a
+            >. Failure to comply may result in disqualification.
+        </p>
+        <h3>3. Submissions</h3>
+        <p>
+            By submitting your code and project, you grant Cam Hack a
+            non-exclusive, royalty-free license to use, reproduce, modify, and
+            publicly display your submission for:
+        </p>
+        <ul class="pl-8 list-disc">
+            <li>Judging and evaluation purposes</li>
+            <li>Promotional and marketing activities related to the event</li>
+        </ul>
+        <h3>4. Intellectual property</h3>
+        <p>
+            You retain ownership to your submission and may use it as you wish,
+            including licensing or commercializing it after the Event.
+        </p>
+        <h3>5. Limitation of liability</h3>
+        <p>
+            The organizers are not liable for any damages or losses incurred
+            during your participation in the Event.
+        </p>
+    </div>
+</section>
