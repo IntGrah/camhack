24eedc6d8886374ca34fa01c0419a34098bfe1aa
IntGrah <148660026+IntGrah@users.noreply.github.com.>
2024-10-17 15:54:02 +0000

    404 page

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,10 @@
+<section>
+    <div class="max-w-7xl mx-auto">
+        <h1 class="mb-8 text-4xl font-bold">
+            404: Not found
+        </h1>
+        <p>
+            <a href="/" class="theme-hyperlink">Click to return home</a>
+        </p>
+    </div>
+</section>
