1bf0dc4525df6f2a224b4e3607684ea6f425a05d
IntGrah <148660026+IntGrah@users.noreply.github.com.>
2024-10-18 11:46:51 +0000

    403 page

---
+++ src/routes/403/+page.svelte
@@ -0,0 +1,10 @@
+<section>
+    <div class="max-w-7xl mx-auto">
+        <h1 class="mb-8 text-4xl font-bold">
+            403: Forbidden
+        </h1>
+        <p>
+            <a href="/" class="theme-hyperlink">Click to return home</a>
+        </p>
+    </div>
+</section>
